fa019d602eab1b16525cc03a300457b3bc905f26
victornavelino <victornavelino@gmail.com>
2024-02-20 13:08:37 -0300

    sdsdsdsd

--- project/apps/stock/views.py
+++ project/apps/stock/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 import json
 from django.core import serializers
-from stock.models import ArticuloDeposito
+from stock.models import ArticuloDeposito, Deposito
 from django.contrib.contenttypes.models import ContentType
 
 # Create your views here.
@@ -20,28 +20,26 @@ def get_articulos_deposito(request, id_deposito):
         print('imprimo objeto id origen en get deposito')
         print(id_objeto)
         obj_get = ct.get_object_for_this_type(pk=id_objeto)
-        
-        articulosdeposito = ArticuloDeposito.objects.filter(deposito=obj_get)
-        if len(articulosdeposito) > 0:
-            results = []
-            for articulo in articulosdeposito:
-                json_valores = {
-                    "id_articulo": str(articulo.pk),
-                    "articulo": articulo.articulo.nombre,
-                    "cantidad": str(articulo.cantidad),
-                    "deposito": articulo.deposito.nombre
-                    }
-                results.append(json_valores)
-            data = json.dumps(results)
-        else:
-            json_valores = {'error': 'El Deposito No tiene articulos'}
-            data = json.dumps(json_valores)
+        if isinstance(obj_get, Deposito):
+            articulosdeposito = ArticuloDeposito.objects.filter(deposito=obj_get)
+            if len(articulosdeposito) > 0:
+                results = [] 
+                for articulo in articulosdeposito:
+                    json_valores = {
+                        "id_articulo": str(articulo.pk),
+                        "articulo": articulo.articulo.nombre,
+                        "cantidad": str(articulo.cantidad),
+                        "deposito": articulo.deposito.nombre
+                        }
+                    results.append(json_valores)
+                data = json.dumps(results)
+            else:
+                json_valores = {'error': 'El Deposito No tiene articulos'}
+                data = json.dumps(json_valores)
         
     else:
         valores = {}
         data = serializers.serialize('json', valores)
-    print('imprmimos data en formato json')
-    print(data)
     return HttpResponse(data, content_type="application/json")
 
 
